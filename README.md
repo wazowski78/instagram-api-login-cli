ab5d83ae371d2515dc607f28cff51e8567389004
Samarth Dave <samarthdave@users.noreply.github.com>
2019-02-04 12:56:40 -0600

    Make directory with shelljs and username, pass validation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # instagram-api-login-cli
 Login and generate a cookie file from the CLI
+
+## Tutorial here:
+[Building an interactive CLI (Medium)](https://codeburst.io/building-a-node-js-interactive-cli-3cb80ed76c86)
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const chalk = require('chalk');
 const inquirer = require('inquirer');
 const figlet = require('figlet');
+const shell = require('shelljs');
 const path = require('path');
-const { V1 } = require('instagram-private-api');
+// const { V1 } = require('instagram-private-api');
 
 const init = () => {
   // Output message to chalk in blue
@@ -20,36 +21,36 @@ const init = () => {
 };
 
 const askQuestions = () => {
-  const questions = [
-    {
-      type: 'input',
-      name: 'USERNAME',
-      message: 'What is your username?'
-    }, {
-      type: 'password',
-      name: 'PASSWORD',
-      mask: '*',
-      message: (prevAnswers) => {
-        return `${prevAnswers.USERNAME}, what is your password?`
+  const questions = [{
+    type: 'input',
+    name: 'USERNAME',
+    message: 'What is your username?',
+    validate: input => (input.length > 0) ? true : 'Enter a valid username.'
+  }, {
+    type: 'password',
+    name: 'PASSWORD',
+    mask: '*',
+    message: (prevAnswers) => {
+      return `${prevAnswers.USERNAME}, what is your password?`
+    },
+    validate: input => (input.length > 0) ? true : 'Enter a valid password.'
+  }, {
+    type: 'input',
+    name: 'FILENAME',
+    default: prevAnswers => `${prevAnswers.USERNAME}.cookie.json`,
+    message: "File name (ends with '.cookie.json')",
+    validate: (input) => {
+      if(input.endsWith('.cookie.json')) {
+        return true;
       }
-    }, {
-      type: 'input',
-      name: 'FILENAME',
-      default: prevAnswers => `${prevAnswers.USERNAME}.cookie.json`,
-      message: "File name (ends with '.cookie.json')",
-      validate: (input) => {
-        if(input.endsWith('.cookie.json')) {
-          return true;
-        }
-        return "File name must end with '.cookie.json'";
-      }
-    }, {
-      type: 'input',
-      name: 'DIRECTORY',
-      message: prevAnswers => `Directory (/cookies/*.cookie.json)`,
-      default: '/passwords/'
+      return "File name must end with '.cookie.json'";
     }
-  ];
+  }, {
+    type: 'input',
+    name: 'DIRECTORY',
+    message: 'Directory (/cookies/)',
+    default: '/cookies/'
+  }];
   return inquirer.prompt(questions);
 };
 
@@ -59,8 +60,18 @@ const run = async () => {
   const answers = await askQuestions();
 
   // TODO: use username and password to login, save cookie to file
-  // const { USERNAME, PASSWORD } = answers;
+  const {
+    USERNAME,
+    PASSWORD,
+    DIRECTORY,
+    FILENAME
+  } = answers;
 
+  // make cookies directory if not exists
+  const fileDirectory = path.join(__dirname, DIRECTORY);
+  shell.mkdir('-p', fileDirectory);
+
+  // TODO: login and make new file inside directory folder with cookie file
 };
 
 run();
